e6b1f834fb0fc98726d8ecbc57e37cd5f5a3888d
TheresaSchmotz <schmotz.theresa@gmail.com>
2018-12-06 03:08:10 +0100

    ich finde den fehler nicht

---
+++ src/Vehicle.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Vehicle {
+
+    static int power ;
+    static double weight;
+    static String name;
+
+    public static void printName(String[]args){
+        System.out.print(name); //Ich hab keine Ahnung was du da formuliert hast, ich versteh das sprachlich nicht.
+    }
+
+    public static void calculatePower(double ps){
+        int ps = JOptionPane.showMessageDialog(power*1.36);
+
+    }
+}
